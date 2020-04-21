6b5d5654dd6d0672bc122676546f6d0d95f4ebdc
Wessel van de Brug <wvdbrug@gmail.com>
2020-04-21 14:32:39 +0200

    Fix tabs om add script

--- add.sh
+++ add.sh
@@ -4,7 +4,7 @@ if [ -z "$1" ]
   then
     echo "No file argument passed"
   else
-  	mvn org.apache.maven.plugins:maven-install-plugin:2.5.2:install-file -Dfile="$1"
+    mvn org.apache.maven.plugins:maven-install-plugin:2.5.2:install-file -Dfile="$1"
     mvn -o org.apache.maven.plugins:maven-install-plugin:2.5.2:install-file -DlocalRepositoryPath="$(dirname $0)/maven/repository" -Dfile="$1"
 fi
 
